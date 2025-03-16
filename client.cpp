1d614b0a9ae4e242a62fd8bbec4a9cecd6fe1642
mayermichael1 <michael.mayer2105@gmail.com>
2025-03-16 13:41:40 +0100

    pull out types

--- client.cpp
+++ client.cpp
@@ -1,18 +1,7 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <wayland-client.h>
 
-typedef uint8_t     u8;
-typedef int8_t      s8;
-typedef uint16_t    u16;
-typedef int16_t     s16;
-typedef uint32_t    u32;
-typedef int32_t     s32;
-typedef uint64_t    u64;
-typedef int64_t     s64;
-
-typedef float       f32;
-typedef double      f64;
+#include "include/types.h"
 
 static void 
 registry_handle_global(void *data, wl_registry *registry, u32 name, const char *interface, u32 version)
