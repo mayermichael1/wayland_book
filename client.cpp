53f64fa8f4f78a19233bc5752f222adb3d74f047
mayermichael1 <michael.mayer2105@gmail.com>
2025-03-16 11:33:40 +0100

    client init

--- client.cpp
+++ client.cpp
@@ -4,5 +4,19 @@
 int 
 main ()
 {
-    return 0;
+    int return_code = 0;
+    struct wl_display *display = wl_display_connect(NULL);
+
+    if (display)
+    {
+        fprintf(stderr, "Connection established!\n");
+        wl_display_disconnect(display);
+    }
+    else
+    {
+        fprintf(stderr, "Failed to connect to Wayland display.\n");
+        return_code = 1;
+    }
+
+    return return_code;
 }
