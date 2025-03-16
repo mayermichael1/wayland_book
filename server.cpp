b85ab99b5dc7b92a6800166a6de4761b21a4af1d
mayermichael1 <michael.mayer2105@gmail.com>
2025-03-16 13:50:54 +0100

    restructure

--- server.cpp
+++ server.cpp
@@ -6,21 +6,18 @@ main()
 {
     int return_code = 0;
     struct wl_display *display = wl_display_create();
+    const char *socket = NULL;
 
     if(display)
     {
-        const char *socket = wl_display_add_socket_auto(display);
-        if(socket)
-        {
-            fprintf(stderr, "Running Wayland Display on %s\n", socket);
-            wl_display_run(display);
-            wl_display_destroy(display);
-        }
-        else
-        {
-            fprintf(stderr, "Unable to add socket to Wayland display.\n");
-            return_code = 1;
-        }
+        socket = wl_display_add_socket_auto(display);
+    }
+
+    if(socket)
+    {
+        fprintf(stderr, "Running Wayland Display on %s\n", socket);
+        wl_display_run(display);
+        wl_display_destroy(display);
     }
     else
     {
